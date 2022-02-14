b7eab0b3c3a5c874946a4830e6acd4db0b687202
HarshMalik <harsh.malik@akqa.com>
2022-02-15 00:42:27 +0530

    text changes

--- src/pages/article/[slug]/index.js
+++ src/pages/article/[slug]/index.js
@@ -1,7 +1,9 @@
 
 import React, { Fragment } from 'react';
 import ArticleDetails from '../../../components/article/articleDetails/index';
+import AccessDenied from '../../../components/shared/accessDenied/index'
 import { getBaseMarkup } from '../../../services/siteContentService';
+import { useSession } from "next-auth/react"
 import { getDirectories } from '../../../utils/utils';
 import path from 'path';
 
